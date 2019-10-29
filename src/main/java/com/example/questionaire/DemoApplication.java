a6e50233cfa0fd824568d764e67ef01f49fbc370
edxuanlen <edxuanlen@gmail.com>
2019-10-29 23:30:18 +0800

    "change package_name"

--- src/main/java/com/example/questionaire/DemoApplication.java
+++ src/main/java/com/example/questionaire/DemoApplication.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.example.questionaire;
 
 
 import org.springframework.boot.SpringApplication;
--- src/main/java/com/example/questionaire/HelloTest.java
+++ src/main/java/com/example/questionaire/HelloTest.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.example.questionaire;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
