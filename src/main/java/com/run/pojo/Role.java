2ee26f509507407c99adc8c201a9901af6a6bb62
edxuanlen <edxuanlen@gmail.com>
2019-11-12 15:24:35 +0800

    "新建Role类映射数据表"

---
+++ src/main/java/com/run/pojo/Role.java
@@ -0,0 +1,17 @@
+package com.run.pojo;
+
+import lombok.Data;
+
+/**
+ * @Author edxuanlen
+ * @PROJECT questionnaire
+ * @Date 2019/11/12 上午10:22
+ * @Version 1.0
+ **/
+
+@Data
+public class Role {
+    private Integer id;
+    private String name;
+}
+
