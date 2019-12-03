b8763489d4fec63e5b5b4d4f031fbfd2b2c6c2d4
edxuanlen <edxuanlen@gmail.com>
2019-12-03 10:24:59 +0800

    "2019.12.3"

--- src/main/java/com/run/controller/QuestionnaireController.java
+++ src/main/java/com/run/controller/QuestionnaireController.java
@@ -4,18 +4,30 @@ import com.run.mapper.QuestionnaireMapper;
 import com.run.pojo.Question;
 import com.run.pojo.QuestionOption;
 import com.run.pojo.Questionnaire;
+import io.swagger.annotations.Api;
+import org.apache.ibatis.jdbc.Null;
+import org.omg.PortableInterceptor.SYSTEM_EXCEPTION;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.math.BigInteger;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import static org.eclipse.jdt.internal.compiler.codegen.ConstantPool.ValueOf;
+
+
 @Controller
 @RequestMapping("/questionnaire")
+@EnableSwagger2 // 启动swagger注解
+// api-value：定义名称，如果没有定义，则默认显示类名
+@Api(value = "Questionnaire Controller", tags = "Questionnaire Control Tag")
 public class QuestionnaireController {
     @Autowired
     QuestionnaireMapper questionnaireMapper;
@@ -30,19 +42,20 @@ public class QuestionnaireController {
         model.addAttribute("allquestions", list);
         //循环每一个问题获取每一个问题的选项
         int total = list.size();
-        BigInteger qId;
-        List<QuestionOption> optionList;
-        Map<BigInteger, List<QuestionOption>> optionsMap = null;
+        BigInteger QId;
+        String s = "options";
+        List<QuestionOption> option_list;
+//        Map<String, List<QuestionOption>> m = null;
+        HashMap<BigInteger, List<QuestionOption>> M = new HashMap<>();
         for(int i = 0; i < total; i ++) {
-            qId = list.get(i).getQId();
-            optionList = questionnaireMapper.getQuestionOptionById(qId);
-
-
-            optionsMap.put(qId,optionList);
-            System.out.println(optionsMap.get(qId));
+            QId = list.get(i).getQId();
+            System.out.println("QID: " + QId);
+            option_list = questionnaireMapper.getQuestionOptionById(QId);
+            System.out.println("option list " + option_list);
+            M.put(QId, option_list);
         }
-       // model.addAttribute("options",optionsMap);
-
+        model.addAttribute(s , M);
+        System.out.println(M);
         return "questionnaire";
     }
 }
